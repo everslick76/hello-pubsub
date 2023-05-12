0225cab1d738000d3e2418526a47b494703136dd
Magnus Appelqvist <magnus.appelqvist@gmail.com>
2023-05-12 15:41:06 +0200

    removed gin

--- main.go
+++ main.go
@@ -1,167 +1,29 @@
-// package main
-
-// import (
-// 	"context"
-// 	"encoding/json"
-// 	"fmt"
-// 	"html/template"
-// 	"log"
-// 	"net/http"
-// 	"sync"
-
-// 	"cloud.google.com/go/pubsub"
-// )
-
-// var (
-// 	topic *pubsub.Topic
-
-// 	// Messages received by this instance.
-// 	messagesMu sync.Mutex
-// 	messages   []string
-// )
-
-// const maxMessages = 10
-
-// func main() {
-// 	ctx := context.Background()
-
-// 	client, err := pubsub.NewClient(ctx, "cloud-core-376009")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer client.Close()
-
-// 	topicName := "hello"
-// 	topic = client.Topic(topicName)
-
-// 	log.Printf("Topic is %s", topic.String())
-
-// 	// Create the topic if it doesn't exist.
-// 	exists, err := topic.Exists(ctx)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	if !exists {
-// 		log.Printf("Topic %v doesn't exist - creating it", topicName)
-// 		_, err = client.CreateTopic(ctx, topicName)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 	}
-
-// 	http.HandleFunc("/", listHandler)
-// 	http.HandleFunc("/pubsub/publish", publishHandler)
-// 	http.HandleFunc("/pubsub/push", pushHandler)
-
-// 	port := "8081"
-// 	log.Printf("Listening on port %s", port)
-// 	if err := http.ListenAndServe(":"+port, nil); err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
-
-// type pushRequest struct {
-// 	Message struct {
-// 		Attributes map[string]string
-// 		Data       []byte
-// 		ID         string `json:"message_id"`
-// 	}
-// 	Subscription string
-// }
-
-// func pushHandler(w http.ResponseWriter, r *http.Request) {
-
-// 	msg := &pushRequest{}
-// 	if err := json.NewDecoder(r.Body).Decode(msg); err != nil {
-// 		http.Error(w, fmt.Sprintf("Could not decode body: %v", err), http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	messagesMu.Lock()
-// 	defer messagesMu.Unlock()
-// 	// Limit to ten.
-// 	messages = append(messages, string(msg.Message.Data))
-// 	if len(messages) > maxMessages {
-// 		messages = messages[len(messages)-maxMessages:]
-// 	}
-// }
-
-// func listHandler(w http.ResponseWriter, r *http.Request) {
-// 	messagesMu.Lock()
-// 	defer messagesMu.Unlock()
-
-// 	if err := tmpl.Execute(w, messages); err != nil {
-// 		log.Printf("Could not execute template: %v", err)
-// 	}
-// }
-
-// func publishHandler(w http.ResponseWriter, r *http.Request) {
-// 	ctx := context.Background()
-
-// 	msg := &pubsub.Message{
-// 		Data: []byte(r.FormValue("payload")),
-// 	}
-
-// 	if _, err := topic.Publish(ctx, msg).Get(ctx); err != nil {
-// 		http.Error(w, fmt.Sprintf("Could not publish message: %v", err), 500)
-// 		return
-// 	}
-
-// 	fmt.Fprint(w, "Message published.")
-// }
-
-// var tmpl = template.Must(template.New("").Parse(`<!DOCTYPE html>
-// <html>
-//   <head>
-//     <title>Pub/Sub</title>
-//   </head>
-//   <body>
-//     <div>
-//       <p>Last ten messages received by this instance:</p>
-//       <ul>
-//       {{ range . }}
-//           <li>{{ . }}</li>
-//       {{ end }}
-//       </ul>
-//     </div>
-//     <form method="post" action="/pubsub/publish">
-//       <textarea name="payload" placeholder="Enter message here"></textarea>
-//       <input type="submit">
-//     </form>
-//     <p>Note: if the application is running across multiple instances, each
-//       instance will have its own list of messages.</p>
-//   </body>
-// </html>`))
-
 package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"sync"
+	"time"
 
 	"cloud.google.com/go/pubsub"
-	"github.com/gin-gonic/gin"
 )
 
 var (
 	topic *pubsub.Topic
+
+	// Messages received by this instance.
+	messagesMu sync.Mutex
+	messages   []string
 )
+
+const maxMessages = 10
 
 func main() {
 
-	// rest
-	router := gin.Default()
-
-	router.GET("/", hello)
-	router.GET("/publish/:msg", publish)
-	
-	err := router.Run(":8081")
-	if err != nil {
-		panic("[Error] failed to start Gin server due to: " + err.Error())
-	}
-
-	//pubsub
 	ctx := context.Background()
 
 	client, err := pubsub.NewClient(ctx, "cloud-core-376009")
@@ -170,36 +32,62 @@ func main() {
 	}
 	defer client.Close()
 
-	topicName := "hello"
-	topic = client.Topic(topicName)
+	topic = client.Topic("hello")
 
 	// check if the topic exists
 	exists, err := topic.Exists(ctx)
 	if err != nil || !exists {
 		log.Fatal(err)
 	}
-}
 
-func hello(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, "Hello from hello-pubsub!")
-}
+	http.HandleFunc("/publish", publishHandler)
+	http.HandleFunc("/push", pushHandler)
 
-func publish(c *gin.Context) {
-
-	msg := c.Param("msg")
-
-	log.Printf("Publishing %s", msg)
-
-	pubsubMsg := &pubsub.Message{
-		Data: []byte(msg),
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatal(err)
 	}
+}
 
-	if _, err := topic.Publish(c, pubsubMsg).Get(c); err != nil {
-		c.IndentedJSON(http.StatusInternalServerError, msg)
+type pushRequest struct {
+	Message struct {
+		Attributes map[string]string
+		Data       []byte
+		ID         string `json:"message_id"`
+	}
+	Subscription string
+}
+
+func pushHandler(w http.ResponseWriter, r *http.Request) {
+
+	msg := &pushRequest{}
+	if err := json.NewDecoder(r.Body).Decode(msg); err != nil {
+		http.Error(w, fmt.Sprintf("Could not decode body: %v", err), http.StatusBadRequest)
 		return
 	}
 
-	log.Printf("Published %s", msg)
+	messagesMu.Lock()
+	defer messagesMu.Unlock()
+	// Limit to ten.
+	messages = append(messages, string(msg.Message.Data))
+	if len(messages) > maxMessages {
+		messages = messages[len(messages)-maxMessages:]
+	}
+}
 
-	c.IndentedJSON(http.StatusOK, msg)
+func publishHandler(w http.ResponseWriter, r *http.Request) {
+
+	ctx := context.Background()
+
+	currentTime := time.Now().String()
+
+	msg := &pubsub.Message{
+		Data: []byte(currentTime),
+	}
+
+	if _, err := topic.Publish(ctx, msg).Get(ctx); err != nil {
+		http.Error(w, fmt.Sprintf("Could not publish message: %v", err), 500)
+		return
+	}
+
+	fmt.Fprint(w, "Message published: "+ currentTime)
 }
