1eb1d23170aec7c26cc29a20c512f7a31ea1f9b6
Magnus Appelqvist <magnus.appelqvist@gmail.com>
2023-09-08 16:33:10 +0200

    testing concurrency

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"sync"
 	"time"
@@ -24,24 +25,28 @@ const maxMessages = 10
 
 func main() {
 
-	ctx := context.Background()
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 
-	client, err := pubsub.NewClient(ctx, "cloud-core-376009")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Close()
+	// ctx := context.Background()
 
-	topic = client.Topic("hello")
+	// client, err := pubsub.NewClient(ctx, "cloud-core-376009")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer client.Close()
 
-	// check if the topic exists
-	exists, err := topic.Exists(ctx)
-	if err != nil || !exists {
-		log.Fatal(err)
-	}
+	// topic = client.Topic("hello")
+
+	// // check if the topic exists
+	// exists, err := topic.Exists(ctx)
+	// if err != nil || !exists {
+	// 	log.Fatal(err)
+	// }
 
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/publish", publishHandler)
+	http.HandleFunc("/concurrency1", concurrency1)
+	http.HandleFunc("/concurrency2", concurrency2)
 	http.HandleFunc("/push", pushHandler)
 
 	if err := http.ListenAndServe(":8081", nil); err != nil {
@@ -93,9 +98,105 @@ func publishHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if _, err := topic.Publish(ctx, msg).Get(ctx); err != nil {
-		http.Error(w, fmt.Sprintf("Could not publish message: %v", err), 500)
+		http.Error(w, fmt.Sprintf("Could not publish message: %v", err), http.StatusBadRequest)
 		return
 	}
 
 	fmt.Fprint(w, "Message published: "+ currentTime)
+}
+
+ // testing goroutines, channels and wait groups options
+
+func concurrency1(w http.ResponseWriter, r *http.Request) {
+
+	// one goroutine per request
+
+	var n int
+
+	requests := r.URL.Query().Get("requests")
+	if _, err := fmt.Sscan(requests, &n); err != nil {
+		http.Error(w, fmt.Sprintf("Could not extract number of requests: %v", err), 500)
+		return
+	}
+
+	ch := make(chan string)
+
+	var wg sync.WaitGroup
+	wg.Add(n)
+
+	go print(ch, &wg)
+
+	for i := 1; i <= n; i++ {
+		go routine1(i, ch, &wg)
+	}
+
+	wg.Wait()
+
+	log.Printf("%s Message(s) published", requests)
+}
+
+func routine1(i int, ch chan<- string, wg *sync.WaitGroup) {
+
+	start := time.Now()
+	ch <- fmt.Sprintf("Processing message number %d", i)
+	time.Sleep(randomDuration(2, 10))
+	ch <- fmt.Sprintf("Published message number %d after %s", i, time.Since(start))
+
+	defer wg.Done()
+}
+
+func concurrency2(w http.ResponseWriter, r *http.Request) {
+
+	// one goroutine per function
+
+	var n int
+
+	requests := r.URL.Query().Get("requests")
+	if _, err := fmt.Sscan(requests, &n); err != nil {
+		http.Error(w, fmt.Sprintf("Could not extract number of requests: %v", err), 500)
+		return
+	}
+
+	ch := make(chan string)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go print(ch, &wg)
+
+	go routine2(n, ch, &wg)
+
+	wg.Wait()
+
+	log.Printf("%s Message(s) published", requests)
+}
+
+func routine2(n int, ch chan<- string, wg *sync.WaitGroup) {
+
+	for i := 1; i <= n; i++ {
+		start := time.Now()
+		ch <- fmt.Sprintf("Processing message number %d", i)
+		time.Sleep(randomDuration(2, 10))
+		ch <- fmt.Sprintf("Published message number %d after %s", i, time.Since(start))
+	}
+
+	close(ch)
+	defer wg.Done()
+}
+
+func randomDuration(min int, max int) time.Duration {
+	return time.Duration(rand.Intn(max - min) + min) * time.Second
+}
+
+func print(ch <-chan string, wg *sync.WaitGroup) {
+
+	for {
+		n, open :=  <-ch
+		if !open {
+			break
+		}
+		log.Printf(n)
+	}
+
+    defer wg.Done()
 }
