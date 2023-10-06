070d33e8579dcbb78bcf4b694346f43143486fc5
Magnus Appelqvist <magnus.appelqvist@gmail.com>
2023-10-06 14:56:49 +0200

    added random delay

--- main.go
+++ main.go
@@ -100,6 +100,8 @@ type jsonResult struct {
 
 func pushHandler(w http.ResponseWriter, r *http.Request) {
 
+	time.Sleep(randomDuration(2, 5))
+
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 
 	msg := &pushRequest{}
